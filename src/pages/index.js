7052fecbdd5ae6d2d2153a3a99008b6bd09d3406
Boost <boostbm@gmail.com>
2021-06-23 10:51:43 -0700

    Updated

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,9 @@ import '../style/main.scss';
 
 export default () => {
   const { title, lang, description, image } = headData;
+  const siteName = "David Yu's Website";
+  const siteUrl = 'https://www.davidyu.dev';
+  const twitterId = '/';
 
   return (
     <>
@@ -17,6 +20,23 @@ export default () => {
         <meta name="description" content={description || "David Yu's Portfolio"} />
         <meta name="image" content={image} />
         <html lang="en" />
+        {/* Social */}
+        <meta itemProp="name" content={siteName} />
+        <meta property="st:title" content={title} />
+        <meta property="st:info" content={description} />
+        {/* OG */}
+        <meta property="og:url" content={siteUrl} />
+        <meta property="og:site_name" content={siteName} />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:image" content={image} />
+        {/* Twitter */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content={title} />
+        <meta name="twitter:creator" content={twitterId} />
+        <meta name="twitter:site" content={twitterId} />
+        <meta name="twitter:description" content={description} />
+        <meta name="twitter:image" content={image} />
       </Helmet>
       <App />
     </>
