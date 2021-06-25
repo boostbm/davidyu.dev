1826b901030c05047fa4c0f46eb0e8fd21314177
Boost <boostbm@gmail.com>
2021-06-25 15:44:56 -0700

    Updated

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,7 +2,7 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: "David Yu's Website", // e.g: 'Name | Developer'
+  title: "Welcome to David Yu's Website", // e.g: 'Name | Developer'
   lang: '', // e.g: en, es, fr, jp
   description: "Welcome to David Yu's Website.", // e.g: Welcome to my website
   image: 'https://davidyu.dev/static/adc1498a16897494283ef420877aa787/89f4f/profile.jpg',
